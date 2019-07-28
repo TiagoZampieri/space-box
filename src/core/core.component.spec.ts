9c72f5f4c4f7c8a07520f0b95dba7ec321178846
zampieri <tzampieri7@gmail.com>
2019-07-28 14:13:18 -0300

    Instalação do angular

---
+++ src/core/core.component.spec.ts
@@ -0,0 +1,35 @@
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CoreComponent } from './core.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        CoreComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(CoreComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'space-box'`, () => {
+    const fixture = TestBed.createComponent(CoreComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('space-box');
+  });
+
+  it('should render title in a h1 tag', () => {
+    const fixture = TestBed.createComponent(CoreComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to space-box!');
+  });
+});
---
+++ src/core/core.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './core-routing.module';
+import { CoreComponent } from './core.component';
+
+@NgModule({
+  declarations: [
+    CoreComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [CoreComponent]
+})
+export class CoreModule { }
